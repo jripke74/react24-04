33a347d3ccd87271720b7c7d8b37ca34536c1502
Jeff Ripke <jripke74@gmail.com>
2024-01-02 13:48:56 -0800

    add modal

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,10 @@
+export default function ResultModal({ result, targetTime }) {
+  return <dialog className="result-modal" open>
+    <h2>You {result}</h2>
+    <p>The target time was <strong>{targetTime}</strong></p>
+    <p>You Stopped the timer with <strong>X seconds left.</strong></p>
+    <form method="dialog">
+      <button>Close</button>
+    </form>
+  </dialog>
+}
