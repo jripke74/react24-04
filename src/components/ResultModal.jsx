a0b9bf685f383e952ab0cab2de8eb0c8f0499085
Jeff Ripke <jripke74@gmail.com>
2024-01-03 07:05:19 -0800

    add time to dialog

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,12 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal(
+  { targetTime, remainingTime, onReset },
+  ref) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   useImperativeHandle(ref, () => {
     return {
@@ -12,11 +17,11 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
   });
 
   return (
-    <dialog ref={dialog} className="result-modal" open>
-      <h2>You {result}</h2>
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You Lost</h2>}
       <p>The target time was <strong>{targetTime}</strong></p>
-      <p>You Stopped the timer with <strong>X seconds left.</strong></p>
-      <form method="dialog">
+      <p>You Stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
